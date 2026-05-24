a0299eaeb7994bd0096401c50d3f5a551d2e7fa7
doer <doersoult@126.com>
2026-05-24 12:40:29 +0800

    update

--- lib/entity/k_line_entity.dart
+++ lib/entity/k_line_entity.dart
@@ -3,39 +3,52 @@ import 'package:k_chart_plus/extension/map_extension.dart';
 import '../entity/k_entity.dart';
 
 class KLineEntity extends KEntity {
+  int? time;
   late double open;
+  late double close;
   late double high;
   late double low;
-  late double close;
   late double vol;
   late double? amount;
-  // late double? turnover;
   double? change;
   double? ratio;
-  int? time;
+
+  double? toRate;
+  double? prevPrice;
+  double? amplitude;
+  double? openPremiumRate;
 
   KLineEntity.fromCustom({
-    this.amount,
+    required this.time,
     required this.open,
     required this.close,
-    this.change,
-    this.ratio,
-    required this.time,
     required this.high,
     required this.low,
     required this.vol,
+    this.amount,
+    this.change,
+    this.ratio,
+    this.toRate,
+    this.prevPrice,
+    this.amplitude,
+    this.openPremiumRate,
   });
 
   KLineEntity.fromJson(Map<String, dynamic> json) {
+    time = json.gInt('time');
     open = json.gDouble('open');
+    close = json.gDouble('close');
     high = json.gDouble('high');
     low = json.gDouble('low');
-    close = json.gDouble('close');
     vol = json.gDouble('vol');
     amount = json.gDouble('amount');
-    time = json.gInt('time');
-    ratio = json.gDouble('ratio');
     change = json.gDouble('change');
+    ratio = json.gDouble('ratio');
+
+    toRate = json.gDouble('toRate');
+    prevPrice = json.gDouble('prevPrice');
+    amplitude = json.gDouble('amplitude');
+    openPremiumRate = json.gDouble('openPremiumRate');
   }
 
   Map<String, dynamic> toJson() {
@@ -47,8 +60,14 @@ class KLineEntity extends KEntity {
     data['low'] = this.low;
     data['vol'] = this.vol;
     data['amount'] = this.amount;
-    data['ratio'] = this.ratio;
     data['change'] = this.change;
+    data['ratio'] = this.ratio;
+
+    data['toRate'] = this.toRate;
+    data['prevPrice'] = this.prevPrice;
+    data['amplitude'] = this.amplitude;
+    data['openPremiumRate'] = this.openPremiumRate;
+
     return data;
   }
 
